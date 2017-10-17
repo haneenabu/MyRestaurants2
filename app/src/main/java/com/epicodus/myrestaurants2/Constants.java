6677148e2770b1bb5d3e55fc8ec942d58e7d3c9d
Guest User <Guest@Epicodus-3E.local>
2017-10-17 11:34:22 -0700

    add contants class and yelp acess key

---
+++ app/src/main/java/com/epicodus/myrestaurants2/Constants.java
@@ -0,0 +1,9 @@
+package com.epicodus.myrestaurants2;
+
+/**
+ * Created by Guest on 10/17/17.
+ */
+
+public class Constants {
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+}
